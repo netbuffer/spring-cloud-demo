380d52ae76c0c3addf0bf41248c88301eda11cab
netbuffer <javawiki@163.com>
2019-08-13 10:31:14 +0800

    test DiscoveryClient

--- user-service-invoker/src/main/java/cn/netbuffer/springclouddemo/userserviceinvoker/client/UserClient.java
+++ user-service-invoker/src/main/java/cn/netbuffer/springclouddemo/userserviceinvoker/client/UserClient.java
@@ -1,5 +1,7 @@
 package cn.netbuffer.springclouddemo.userserviceinvoker.client;
 
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,13 +9,27 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 @RestController
 @RequestMapping("/invoke")
 public class UserClient {
 
     @Resource
+    private DiscoveryClient discoveryClient;
+
+    @Resource
     private RestTemplate restTemplate;
+
+    @GetMapping("instances/{serviceId}")
+    public List<ServiceInstance> getInstances(@PathVariable("serviceId") String serviceId) {
+        return discoveryClient.getInstances(serviceId);
+    }
+
+    @GetMapping("discoveryclient")
+    public DiscoveryClient getDiscoveryClient() {
+        return discoveryClient;
+    }
 
     @GetMapping("/user/{id}")
     public String getUser(@PathVariable("id") Long id) {
