63c2cd0764d4d1fe4e901dafc38c48391176d237
netbuffer <javawiki@163.com>
2019-08-28 09:42:57 +0800

    update

--- user-service-invoker/src/main/java/cn/netbuffer/springclouddemo/userserviceinvoker/service/UserService.java
+++ user-service-invoker/src/main/java/cn/netbuffer/springclouddemo/userserviceinvoker/service/UserService.java
@@ -1,11 +1,17 @@
 package cn.netbuffer.springclouddemo.userserviceinvoker.service;
 
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.math.RandomUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import javax.annotation.Resource;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.concurrent.TimeUnit;
 
+@Slf4j
 @Service
 public class UserService {
 
@@ -14,10 +20,21 @@ public class UserService {
 
     @HystrixCommand(fallbackMethod = "getUserFallback")
     public String getUser(Long id) {
-        return restTemplate.getForObject("http://user-service-provider/user/" + id, String.class);
+        int sleep = RandomUtils.nextInt(6);
+        log.info("invoke getUser {},sleep {} s", id, sleep);
+        //模拟请求耗时测试熔断
+        try {
+            TimeUnit.SECONDS.sleep(sleep);
+        } catch (InterruptedException e) {
+            log.error("sleep error:{}", e.getMessage());
+        }
+        String r = restTemplate.getForObject("http://user-service-provider/user/" + id, String.class);
+        log.info("after getUser {},result:{}", id, r);
+        return r;
     }
 
     public String getUserFallback(Long id) {
-        return id + " error";
+        log.error("getUserFallback {}", id);
+        return id + " error at " + LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
     }
 }
