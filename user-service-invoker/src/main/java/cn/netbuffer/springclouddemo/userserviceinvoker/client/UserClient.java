250d681a7c8753f5e1b729b3ccd3926462b1cd7e
netbuffer <javawiki@163.com>
2019-08-29 15:28:15 +0800

    test feign fallback

--- user-service-invoker/src/main/java/cn/netbuffer/springclouddemo/userserviceinvoker/client/UserClient.java
+++ user-service-invoker/src/main/java/cn/netbuffer/springclouddemo/userserviceinvoker/client/UserClient.java
@@ -5,9 +5,22 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient("user-service-provider")
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+/**
+ * fallback参数
+ */
+@FeignClient(value = "user-service-provider", fallback = UserClient.UserClientFallback.class)
 public interface UserClient {
 
     @GetMapping("/user/{id}")
     String getUser(@PathVariable("id") Long id, @RequestParam Integer s);
+
+    class UserClientFallback implements UserClient {
+        @Override
+        public String getUser(Long id, Integer s) {
+            return id + ":" + s + ":error at" + LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+        }
+    }
 }
