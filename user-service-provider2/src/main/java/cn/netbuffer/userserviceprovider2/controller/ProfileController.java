5bf6874b89907797a77b7f6d25dcfd54bc5e87b8
netbuffer <javawiki@163.com>
2019-09-03 11:33:36 +0800

    update

--- user-service-provider2/src/main/java/cn/netbuffer/userserviceprovider2/controller/ProfileController.java
+++ user-service-provider2/src/main/java/cn/netbuffer/userserviceprovider2/controller/ProfileController.java
@@ -1,20 +1,23 @@
 package cn.netbuffer.userserviceprovider2.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
+ * @RefreshScope配置信息刷新能力
  * @author admin
  * @date 2019/09/02
  */
+@RefreshScope
 @RestController
 @RequestMapping("/profile")
 public class ProfileController {
 
-    //https://github.com/spring-cloud-samples/config-repo/blob/master/application-dev.yml
-    @Value("${my.prop}")
+    //https://github.com/netbuffer/spring-cloud-config-repo-demo/blob/master/user-service-provider2-dev.yml
+    @Value("${spring.application.name}")
     private String prop;
 
     @GetMapping("prop")
