bc0c57c33e3fc8621359e2c3f99f258400b1e25e
netbuffer <javawiki@163.com>
2019-09-03 13:42:34 +0800

    update

--- user-service-provider2/README.md
+++ user-service-provider2/README.md
@@ -1,1 +1,5 @@
-* 也可使用命令行参数/环境变量等指定bootstrap.yml中的配置参数
+> 也可使用命令行参数/环境变量等指定bootstrap.yml中的配置参数
+
+### test
+* http://localhost:8801/profile/env/server.port
+* http://localhost:8801/profile/env/spring.application.name
--- user-service-provider2/src/main/java/cn/netbuffer/userserviceprovider2/controller/ProfileController.java
+++ user-service-provider2/src/main/java/cn/netbuffer/userserviceprovider2/controller/ProfileController.java
@@ -1,14 +1,17 @@
 package cn.netbuffer.userserviceprovider2.controller;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Resource;
+
 /**
- * @RefreshScope配置信息刷新能力
  * @author admin
+ * @RefreshScope配置信息刷新能力
  * @date 2019/09/02
  */
 @RefreshScope
@@ -17,11 +20,21 @@ import org.springframework.web.bind.annotation.RestController;
 public class ProfileController {
 
     //https://github.com/netbuffer/spring-cloud-config-repo-demo/blob/master/user-service-provider2-dev.yml
-    @Value("${spring.application.name}")
-    private String prop;
+    @Resource
+    private Environment environment;
 
-    @GetMapping("prop")
-    public String getProp() {
-        return prop;
+    @GetMapping("env/{prop}")
+    public String getProp(@PathVariable String prop) {
+        return environment.getProperty(prop);
+    }
+
+    @GetMapping("active")
+    public String[] active() {
+        return environment.getActiveProfiles();
+    }
+
+    @GetMapping("default")
+    public String[] defaultProfile() {
+        return environment.getDefaultProfiles();
     }
 }
