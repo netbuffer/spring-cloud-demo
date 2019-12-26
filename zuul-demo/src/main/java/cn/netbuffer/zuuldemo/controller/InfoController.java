b29d3600b5816ca2d1fe81c87231ea5fd22c974a
netbuffer <javawiki@163.com>
2019-12-26 15:21:33 +0800

    update

---
+++ zuul-demo/src/main/java/cn/netbuffer/zuuldemo/controller/InfoController.java
@@ -0,0 +1,19 @@
+package cn.netbuffer.zuuldemo.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/info")
+public class InfoController {
+
+    @Value("${hystrix.command.default.execution.isolation.thread.timeoutInMilliseconds}")
+    private String hystrixTimeout;
+
+    @GetMapping("hystrixTimeout")
+    public String getHystrixTimeout() {
+        return hystrixTimeout;
+    }
+}
