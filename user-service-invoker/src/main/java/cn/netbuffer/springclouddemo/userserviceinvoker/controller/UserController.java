c5897567708ca1485da5f570a4839ff05eda47cb
netbuffer <javawiki@163.com>
2021-11-11 22:14:22 +0800

    test discoveryClient method

--- user-service-invoker/src/main/java/cn/netbuffer/springclouddemo/userserviceinvoker/controller/UserController.java
+++ user-service-invoker/src/main/java/cn/netbuffer/springclouddemo/userserviceinvoker/controller/UserController.java
@@ -7,7 +7,6 @@ import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
-
 import javax.annotation.Resource;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
@@ -32,6 +31,21 @@ public class UserController {
     @GetMapping("instances/{serviceId}")
     public List<ServiceInstance> getInstances(@PathVariable("serviceId") String serviceId) {
         return discoveryClient.getInstances(serviceId);
+    }
+
+    @GetMapping("getOrder")
+    public int getOrder() {
+        return discoveryClient.getOrder();
+    }
+
+    @GetMapping("description")
+    public String description() {
+        return discoveryClient.description();
+    }
+
+    @GetMapping("getServices")
+    public List<String> getServices() {
+        return discoveryClient.getServices();
     }
 
     @GetMapping("discoveryclient")
