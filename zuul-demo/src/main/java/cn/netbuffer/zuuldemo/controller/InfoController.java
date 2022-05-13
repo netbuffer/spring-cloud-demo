680ff9afb53fa834a90a337d442a2dab4ea252f8
netbuffer <javawiki@163.com>
2022-05-13 16:08:06 +0800

    add environment/property

--- zuul-demo/src/main/java/cn/netbuffer/zuuldemo/controller/InfoController.java
+++ zuul-demo/src/main/java/cn/netbuffer/zuuldemo/controller/InfoController.java
@@ -1,9 +1,11 @@
 package cn.netbuffer.zuuldemo.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import javax.annotation.Resource;
 
 @RestController
 @RequestMapping("/info")
@@ -12,9 +14,17 @@ public class InfoController {
     @Value("${hystrix.command.default.execution.isolation.thread.timeoutInMilliseconds:null}")
     private String hystrixTimeout;
 
+    @Resource
+    private Environment environment;
+
     @GetMapping("hystrixTimeout")
     public String getHystrixTimeout() {
         return hystrixTimeout;
+    }
+
+    @GetMapping("environment/property")
+    public String environment(String property) {
+        return environment.getProperty(property);
     }
 
 }
